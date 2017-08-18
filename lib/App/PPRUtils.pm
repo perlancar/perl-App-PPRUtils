0b6e0be6a7a6419868502dcfab0983790b17d48b
perlancar (on pc-office) <perlancar@gmail.com>
2017-08-18 13:36:26 +0700

    Initial commit

---
+++ lib/App/PPRUtils.pm
@@ -0,0 +1,27 @@
+package App::PPRUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+$SPEC{pprgrep} = {
+};
+sub pprgrep {
+};
+
+1;
+# ABSTRACT: Command-line utilities related to PPR
+
+=head1 SYNOPSIS
+
+This distribution provides the following command-line utilities related to
+L<PPR>:
+
+#INSERT_EXECS_LIST
+
+=cut
